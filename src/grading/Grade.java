d5708accaa027e7309bb7688ae37d80a886152d3
jimbrey300 <jimbrey300@ymail.com>
2021-10-04 16:53:24 -0700

    wheres the taco bell huh?

--- src/grading/Grade.java
+++ src/grading/Grade.java
@@ -1,6 +1,41 @@
 package grading;
 
-public class Grade {
-	
-//tacobell...
+import java.util.List;
+import grading.SizeException;
+
+public class Grade
+{
+  private string key
+  private double value
+    
+   public Grade(String key)
+   {
+	  
+   }
+  
+   public Grade (String key, double value)
+   {
+	   
+   }
+   
+   public Grade (String key, Double value)
+   {
+	   
+   }
+   
+   public String getKey()
+   {
+	 
+   }
+   
+   public Double getValue()
+   {
+	   
+   }
+   
+   public String toString()
+   {
+	   
+   } 
+    
 }
