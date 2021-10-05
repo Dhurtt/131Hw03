08cf83d61494dc429cb9517c1d8099c06ba13fdf
dalen <dalen@DESKTOP-JGNB6OA>
2021-10-04 17:51:34 -0700

    wts

--- src/grading/weightedTotalStrategy.java
+++ src/grading/weightedTotalStrategy.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.lang.String;
 import java.lang.Double;
 
-public class weightedTotalStrategy extends GradingStrategy()
+public class weightedTotalStrategy extends GradingStrategy
 {
 	Map<String, Double> weights = new Hashmap<String, Double>();
 	public weightedTotalStrategy()
