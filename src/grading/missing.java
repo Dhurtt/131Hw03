52bb23036e0f8392825ce4ed486a38bb6877bffb
dalen <dalen@DESKTOP-JGNB6OA>
2021-10-04 17:41:55 -0700

    missing.java, still dont have doubleValue

--- src/grading/missing.java
+++ src/grading/missing.java
@@ -4,6 +4,13 @@ public class missing
 {
 	private final double DEFAULT_MISSING_VALUE = 0;
 	
-	
+	public double doubleValue(double number)
+	{
+		return number * 2;
+	}
+	public double doubleValue(double number, double missingValue)
+	{
+		
+	}
 	
 }
