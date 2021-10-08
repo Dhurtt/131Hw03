b0404dbbd323df45dccf3d10b36612f429fc62d0
dalen <dalen@DESKTOP-JGNB6OA>
2021-10-07 17:06:41 -0700

    GS update

--- src/grading/GradingStrategy.java
+++ src/grading/GradingStrategy.java
@@ -1,16 +1,11 @@
 package grading;
 
-import java.util.List;
 import java.util.Map;
 
-import grading.SizeException;
 
 public interface GradingStrategy
 {
-	public class calculate key(String , grades)
-	{
-		
-	}
+
 }
 
 
